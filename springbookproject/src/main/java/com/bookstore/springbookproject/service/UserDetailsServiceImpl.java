2d62c61df649fd363c9654c0a37b75efc9aa72e7
nkatajoshua <93196162+nkatajoshua@users.noreply.github.com>
2024-04-19 00:57:16 +0300

    Revert "Authen and Authorize"
    This reverts commit 41d45c4fbc2fec36a11de12456885d3789df4c8d.

--- springbookproject/src/main/java/com/bookstore/springbookproject/service/UserDetailsServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package com.bookstore.springbookproject.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserDetailsServiceImpl extends UserDetailsService {
-}
